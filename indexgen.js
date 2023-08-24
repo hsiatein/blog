4ead409fc600997fe9689aacd22c7d111fca19a8
hsiatein <116558762+hsiatein@users.noreply.github.com>
2023-08-25 02:13:21 +0800

    Add files via upload

--- indexgen.js
+++ indexgen.js
@@ -1,15 +1,16 @@
 const files = [
-    { name: "实现简单的网络爬虫"},
-  ];
+  "实现简单的网络爬虫",
+  "估计1形状类似Watson引理的积分估计"
+];
 const ol = document.createElement("ol");
 files.forEach((file) => {
   const li = document.createElement("li");
   const a = document.createElement("a");
   li.className="index";
-  a.textContent = file.name;
+  a.textContent = file;
   a.href = "javascript:void(0)";
   a.onclick=function() {
-    toggleIframe('preview', "pages/"+file.name+".html");
+    toggleIframe('preview', "pages/"+file+".html");
   };
   li.appendChild(a);
   ol.appendChild(li);
